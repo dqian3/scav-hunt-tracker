601a0327b4fb653e5f5f0f866fa8620cc483d100
Daniel Qian <daniel.qian@nyu.edu>
2023-09-26 19:25:08 -0400

    try huggingface endpoint

--- src/App.js
+++ src/App.js
@@ -2,15 +2,80 @@ import React, { useEffect, useState } from 'react';
 
 import { db } from './firebase'
 
-import { collection, getDocs, query, where, doc} from "firebase/firestore"; 
+import { collection, getDocs, doc, updateDoc} from "firebase/firestore"; 
+
+// Todo update stuff in general
+
+async function getItemGuess(image, labels) {
+  const response = await fetch("https://api-inference.huggingface.co/models/openai/clip-vit-base-patch32", {
+    method: "POST",
+    body: JSON.stringify({
+      image,
+      parameters: {
+        candidate_labels: labels
+      }    
+    })
+  });
+  const answer = await response.json();
+  console.log(answer);
+
+}
+
+async function convertBase64(file){
+  /// ewwwww
+  return await new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file)
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    }
+    fileReader.onerror = (error) => {
+      reject(error);
+    }
+  })
+}
+
+
+
+function EditHunt({curHunt}) {
+  const [items, setItems] = useState("");
+
+  console.log(curHunt)
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const huntRef = doc(db, "hunts", curHunt);
+    await updateDoc(huntRef, {
+      items: items.split("\n").map((item) => ({
+        description: item.trim()
+      }))
+    });
+  }
+
+  return <form onSubmit={handleSubmit}> 
+    <h3>Set items</h3>
+
+    <p>Items, separated by line</p>
+    <textarea value={items} onChange={(e) => setItems(e.target.value)}/>
+
+    <br/>
+    <button type="submit">Submit</button>
+  </form>
+
+
+}
 
 function App() {
-
-  let [hunts, setHunts] = useState([]);
+  // State from firebase
+  let [hunts, setHunts] = useState({});
   let [players, setPlayers] = useState([]);
 
+  // Form state
   let [curPlayer, setCurPlayer] = useState(localStorage.getItem("player") || "");
-  let [curHunt, setCurHunt] = useState(0);
+  let [curHunt, setCurHunt] = useState("");
+
+  let [image, setImage] = useState(null);
+  let [imageObjURL, setImageObjURL] = useState("");
 
   useEffect(() => {
     async function getData() {
@@ -18,12 +83,25 @@ function App() {
       setPlayers(results.docs.map((doc) => doc.id));
 
       results = await getDocs(collection(db, "hunts"));
-      setHunts(results.docs);
-
-      console.log(results.docs.at(0).data())
+      setHunts(Object.fromEntries(results.docs.map((doc) => [doc.id, doc.data()])));
     }
     getData();
   }, []);
+
+
+  function handleChangeImage (e) {
+    setImage(e.target.files[0]);
+    setImageObjURL(URL.createObjectURL(e.target.files[0]))
+  };
+
+
+
+  async function guessItem(e) {
+    e.preventDefault();
+    const labels = hunts[curHunt].items.slice(0, 10);
+    const encodedImage = await convertBase64(image);
+    getItemGuess(encodedImage, labels);
+  }
 
   return (
     <div className="App">
@@ -32,17 +110,19 @@ function App() {
       </h1>
 
       <div>
-        <h3>Upload item</h3>
 
-        <form>
+        <p>
           What scavenger hunt? 
           <select 
             value={curHunt}
-            onChange={(e) => e.setCurHunt(e.target.value)}
+            onChange={(e) => setCurHunt(e.target.value)}
           >
-            {hunts.map((h, i) => <option key={h.id} value={i}>{h.id}</option>)}
+            <option value={""}>Select...</option>
+            {Object.keys(hunts).map((h) => <option key={h} value={h}>{h}</option>)}
           </select>
+        </p>
 
+        <p>
           Who are you: 
           <select
             value={curPlayer}
@@ -51,10 +131,22 @@ function App() {
           >
             {players.map((n) => <option key={n} value={n}>{n}</option>)}
           </select>
+        </p>
 
+        {
+          (curHunt !== "" && hunts[curHunt]?.items.length === 0) &&
+          <EditHunt curHunt={curHunt}></EditHunt>
+        }
+
+        <h3>Upload item</h3>
+
+        <form>
+          <img alt="pending image" height={200} src={imageObjURL}/>
           <br/>          
-          <input name='image' type="file" accept="image/*" capture="environment">
+          <input name='image' type="file" accept="image/*" capture="environment" onChange={handleChangeImage}>
           </input>
+
+          <button onClick={guessItem}>Guess</button>
 
         </form>
 
@@ -69,7 +161,7 @@ function App() {
 
           <tbody>
           {
-            hunts[curHunt]?.data().items.map((item) => <tr key={item["description"]}>
+            hunts[curHunt]?.items.map((item) => <tr key={item["description"]}>
               <td>{item["description"]}</td>
               <td>Upload</td>
             </tr>) 
@@ -83,5 +175,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
